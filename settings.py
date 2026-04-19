28067f42ed11dde3db4d7143c3cf37d08b9071ea
Gaven <ghuynh@student.cscc.edu>
2026-04-19 19:46:42 -0400

    Create alien width and height in settings

--- settings.py
+++ settings.py
@@ -21,5 +21,7 @@ class Settings:
         self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
         self.fleet_speed = 5
     
