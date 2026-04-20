ce839b1d64bce009bd6a52ef4fb7f9c351f6952e
Gaven <ghuynh@student.cscc.edu>
2026-04-19 20:14:52 -0400

    If alien hits a boundry, move it "down" y axis

--- alien.py
+++ alien.py
@@ -28,6 +28,7 @@ class Alien(Sprite):
 
         if self.check_edges():
             self.settings.fleet_direction *= -1
+            self.y += self.settings.fleet_drop_speed
 
         self.x += temp_speed * self.settings.fleet_direction
         self.rect.x = self.x
